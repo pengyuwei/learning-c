ce9ea62d3a109b0ff7bbc4da6bb8b5316215bb1e
0xff <pengyuwei@gmail.com>
2022-08-21 21:13:01 +0800

    input

---
+++ C/base/input.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <assert.h>
+
+void test_fgets() {
+    char content[60] = {0};
+    FILE * fp = fopen("readme.md", "r");
+    char * r = fgets(content, sizeof(content), fp);
+    if (NULL != r) {
+        // "hello\n", '\000' <repeats 53 times>
+        puts(content);
+        assert(content[5] == '\n');
+    }
+    fclose(fp);
+}
+
+void test_getc() {
+    char c = '\0'; 
+    // getc只接收一个字符，如果输入多个，会自动缓冲给下面的getc
+    printf("input a char:");
+    c = getc(stdin);
+    putc(c, stdout);
+    c = getc(stdin);
+    putc(c, stdout);
+}
+
+int main() {
+    test_fgets();
+    test_getc();
+
+    return 0;
+}
