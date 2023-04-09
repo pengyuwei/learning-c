a28c26426c81df6c4b04c61a0f7222ddb85958c4
0xff <pengyuwei@gmail.com>
2023-04-09 21:29:15 +0800

    fixbug

--- CPP/stl/set.cpp
+++ CPP/stl/set.cpp
@@ -1,7 +1,17 @@
 #include <unordered_set>
+#include <string>
+#include <cstdio>
 #include <assert.h>
 
 typedef std::unordered_set<int > UoSet;
+
+std::string set2string(UoSet& set) {
+    std::string ret;
+    for (UoSet::iterator i = set.begin(); i != set.end(); i++) {
+        ret += *i;
+    }
+    return ret;
+}
 
 // c++ -Wall -g -std=c++11 set.cpp -o set.out
 int main() {
@@ -24,7 +34,9 @@ int main() {
     assert(set.bucket_size(3) == 1);
 
     UoSet::iterator ret = set.find(3);
-    printf("find: %d\n", *ret);
+    printf("%s find: %d\n", set2string(set).c_str(), *ret);
+    UoSet::iterator ret2 = set.find(99);
+    assert(ret2 == set.end());
     set.erase(3);
     set.clear();
 
