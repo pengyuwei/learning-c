5eec993881da878cb881c638b51e6e8951c6d3f9
0xff <pengyuwei@gmail.com>
2023-04-20 00:04:58 +0800

    C++Concurrency example code

---
+++ CPP/thread/concurrency/listing_2.2.cpp
@@ -0,0 +1,42 @@
+#include <thread>
+
+void do_something(int& i) {
+    ++i;
+}
+
+struct func {
+    int& i;
+
+    func(int& i_):i(i_){}
+
+    void operator()() {
+        for(unsigned j=0;j<1000000;++j) {
+            do_something(i);
+        }
+    }
+};
+
+void do_something_in_current_thread() {
+    printf("do_something_in_current_thread\n");
+}
+
+void f() {
+    int some_local_state = 0;
+    func my_func(some_local_state);
+    std::thread t(my_func);
+
+    try {
+        do_something_in_current_thread();
+    }
+    catch(...) {
+        t.join();
+        throw;
+    }
+    
+    t.join();
+}
+
+// c++ -Wall -g -std=c++11 listing_2.2.cpp -lpthread 
+int main() {
+    f();
+}
